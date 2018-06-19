3fd57ecf06a5a7e453eca4510589b4ea2973cf0c
MoritzOsterDaum <39740708+MoritzOsterDaum@users.noreply.github.com>
2018-06-19 18:58:42 +0200

    Add files via upload

---
+++ Beep.sound.py
@@ -0,0 +1,36 @@
+import winsound	
+Abstand = 151
+while Abstand >= 150:
+	pass
+while Abstand >= 100:
+		frequency = 525
+		duration = 999	
+		winsound.Beep(frequency, duration)
+while Abstand >= 75:
+		frequency = 525
+		duration = 850	
+		winsound.Beep(frequency, duration)
+while Abstand >= 50:
+		frequency = 525
+		duration = 600
+		winsound.Beep(frequency, duration)
+while Abstand >= 40:
+		frequency = 525
+		duration = 500	
+		winsound.Beep(frequency, duration)
+while Abstand >= 30:
+		frequency = 525
+		duration = 400	
+		winsound.Beep(frequency, duration)
+while Abstand >= 20:
+		frequency = 525
+		duration = 300
+		winsound.Beep(frequency, duration)
+while Abstand >= 15:
+		frequency = 525
+		duration = 200	
+		winsound.Beep(frequency, duration)
+while Abstand >= 0:
+		frequency = 525
+		duration = 10000
+		winsound.Beep(frequency, duration)
